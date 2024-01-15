d206a5759a8606206197e3bbeb3bfe2489130f3c
Nico Huettmann <nico.huettmann@embl.de>
2024-01-15 14:18:27 +0100

    Push first files

---
+++ README.md
@@ -0,0 +1,6 @@
+# pOmics-handbook
+
+
+Handbook for proteomics data analysis in R with the [pOmics3](https://github.com/nicohuttmann/pOmics3) package: https://nicohuttmann.github.io/pOmics3-handbook/
+
+
---
+++ _render.R
@@ -0,0 +1,19 @@
+
+
+
+bookdown::render_book(input = "index.Rmd",
+                      output_format = "bookdown::gitbook",
+                      config_file = "_bookdown.yml",
+                      preview = FALSE)
+
+
+
+
+# render_book(input = ".", output_format = NULL, ..., clean = TRUE,
+#             envir = parent.frame(), clean_envir = !interactive(),
+#             output_dir = NULL, new_session = NA, preview = FALSE,
+#             config_file = "_bookdown.yml")
+
+
+
+
